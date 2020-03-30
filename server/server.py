92ac3371f8d742296cc681738f457c70c39ff012
Tim Cussins <tim.cussins@aviatnet.com>
2020-03-30 22:54:19 +1300

    Allow clients (plural) to add/delete fragments

--- server/server.py
+++ server/server.py
@@ -1,14 +1,27 @@
 import asyncio
 import websockets
+import json
 
 clients = set()
+fragments = list()
 
-async def broadcast(data):
-    for ws in clients:
+def makeDoc():
+    doc = dict()
+    doc["name"]         = "demo"
+    doc["fragments"]    = list()
+
+    for f in fragments:
+        doc["fragments"].append(f)
+
+    return json.dumps(doc)
+
+async def update():
+    doc = makeDoc()
+    for c in clients:
         try:
-            await ws.send(data)
+            await c.send(doc)
         except websockets.exceptions.ConnectionClosed:
-            pass
+            clients.remove(c)
 
 async def conn(reader, writer):
     _ = await reader.read(1024)
@@ -28,12 +41,45 @@ async def webserver(host, port):
         await srv.serve_forever()
 
 async def echo(websocket, path):
+
     clients.add(websocket)
     try:
         while True:
             data = await websocket.recv()
-            await broadcast("alert")
-            await websocket.send(data + " reply!")
+
+            j = json.loads(data)
+
+            action = j["action"]
+
+            if action  == "attach":
+
+                print("attach")
+                await websocket.send(makeDoc())
+
+            elif action == "add_fragment":
+
+                print("add_fragment")
+                fragment = j["fragment"]
+                print(fragment)
+                fragments.append(fragment)
+                await update()
+
+            elif action == "del_fragment":
+
+                print("del_fragment")
+                fragment = j["fragment"]
+                print(fragment)
+
+                for f in fragments:
+                    if f == fragment:
+                        fragments.remove(f)
+                        break
+
+                await update()
+
+            else:
+                print("unknown command")
+
     except websockets.exceptions.ConnectionClosed:
         clients.remove(websocket)
 
